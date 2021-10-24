987d7fbb0b649bdeb5b96db5d61f72e53683c5e3
Harvey Appleton <info@harveyappleton.co.uk>
2021-10-24 18:39:53 +0100

    Update metro.config.js

--- example/metro.config.js
+++ example/metro.config.js
@@ -6,7 +6,7 @@ const pak = require('../package.json');
 const root = path.resolve(__dirname, '..');
 
 const modules = Object.keys({
-  ...pak.peerDependencies,
+  ...pak.peerDependencies
 });
 
 module.exports = {
@@ -16,25 +16,20 @@ module.exports = {
   // We need to make sure that only one version is loaded for peerDependencies
   // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
+    blacklistRE: blacklist(modules.map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))),
 
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
-    }, {}),
+    }, {})
   },
 
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+        inlineRequires: true
+      }
+    })
+  }
 };
